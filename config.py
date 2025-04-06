5d586bc0a80a04da87eee0ae14cfbd535de7dd20
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-04-06 13:22:35 +0200

    Updated requirements.txt, Updated config.py
    Updated SQL-connector version.
    Added env variables to config file.

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ DB_DETAILS = {
             'DB_TYPE': 'mysql',
             'DB_HOST': 'localhost',
             'DB_NAME': 'retail_db',
-            'DB_USER': 'root',
-            'DB_PASS': 'root',
+            'DB_USER': os.environ.get('SOURCE_DB_USER'),
+            'DB_PASS': os.environ.get('SOURCE_DB_PASS'),
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
             'DB_HOST': 'localhost',
             'DB_NAME': 'retail_db',
-            'DB_USER': 'root',
-            'DB_PASS': 'root',
+            'DB_USER': os.environ.get('TARGET_DB_USER'),
+            'DB_PASS': os.environ.get('TARGET_DB_PASS'),
         }
     }
 }
