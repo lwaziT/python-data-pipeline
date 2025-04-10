f364bc307d1391ac2777f1120f39ef8b4b5a1315
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-04-10 16:25:19 +0200

    Added logic to create connection to MySQL database

--- util.py
+++ util.py
@@ -1,5 +1,27 @@
 import pandas as pd
 
+from mysql import connector as mc
+from mysql.connector import errorcode as ec
+from config import DB_DETAILS
+
+def load_db_details(env):
+    return DB_DETAILS[env]
+
+def get_mysql_connection(db_host, db_name, db_user, db_pass):
+    try:
+        connection = mc.connect(
+            user=db_user,
+            password=db_pass,
+            host=db_host,
+            database=db_name
+        )
+    except mc.Error as error:
+        if error.errno == ec.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        else:
+            print(error)
+    return connection
+
 def get_tables(path):
     tables = pd.read_csv(path, sep=':')
     return tables.query('to_be_loaded == "yes"')
